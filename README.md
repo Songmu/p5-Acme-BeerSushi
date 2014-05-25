8190a6d020074d228670d1d2e81d40cd517c22f8
Songmu <y.songmu@gmail.com>
2014-05-25 18:45:26 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::BeerSushi - It's new $module
+
+# SYNOPSIS
+
+    use Acme::BeerSushi;
+
+# DESCRIPTION
+
+Acme::BeerSushi is ...
+
+# LICENSE
+
+Copyright (C) Songmu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Songmu <y.songmu@gmail.com>
---
+++ lib/Acme/BeerSushi.pm
@@ -0,0 +1,39 @@
+package Acme::BeerSushi;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::BeerSushi - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::BeerSushi;
+
+=head1 DESCRIPTION
+
+Acme::BeerSushi is ...
+
+=head1 LICENSE
+
+Copyright (C) Songmu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Songmu E<lt>y.songmu@gmail.comE<gt>
+
+=cut
+
